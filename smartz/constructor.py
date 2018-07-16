dca0165c743384c165a93e79ad963b565379fc85
Boogerwooger <boogerwooger@mail.ru>
2018-07-16 17:59:51 +0300

    step

--- smartz/constructor.py
+++ smartz/constructor.py
@@ -28,12 +28,12 @@ class Constructor(ConstructorInstance):
                     "$ref": "#/definitions/address"                                                                                                              
                 },
                 "tokenDecimals": {                                                                                                                                       
-                    "title": "Token decimals",                                                                                                                           
+            "title": "Token decimals",                                                                                                                           
                     "description": "Token decimals count (0..255)",                                                                                                      
                     "type": "integer",
-		    "default": 0,                                                                                                                                  
-                    "minLength": 0,                                                                                                                                      
-                    "maxLength": 4                                                                                                                                     
+            "default": 2,                                                                                                                                  
+                    "minimum": 0,                                                                                                                                      
+                    "maximum": 18                                                                                                                                     
                 },                  
                 "merkleRoot": {
                     "description": "Just type text, hash (keccak256) of it will be sent",
@@ -403,30 +403,114 @@ contract MintableToken is StandardToken, Ownable {
     totalSupply_ = totalSupply_.add(_amount);
     balances[_to] = balances[_to].add(_amount);
     emit Mint(_to, _amount);
-    
+  }   
+}
+
+contract MerkleAirdrop {
+
+    address owner;
+    bytes32 public merkleRoot;
+
+    // address of contract, having "transfer" function 
+    // airdrop contract must have ENOUGH TOKENS in its balance to perform transfer
+    MintableToken token_contract;
+
+    // fix already minted addresses
+    mapping (address => bool) spent;
     event AirdropTransfer(address addr, uint256 num);
 
-    constructor() public {
+    constructor(address _token_contract, bytes32 _merkleRoot) public {
         owner = msg.sender;
-        num_tokens_to_transfer = %num_tokens_to_transfer%;
-        token_contract = %token_address%;
-        merkle_root = %merkle_root%;
-        %payment_code%
+        token_contract = MintableToken(_token_contract);
+        merkleRoot = _merkleRoot;
     }
 
-    function mint_by_merkle_proof(bytes32[] proof, address who) public returns(bool) {
-        require(spent[who] != true);
+    function setRoot(bytes32 _merkleRoot) public { // onlyOwner [FIXME]
+        require(msg.sender == owner);
+        require(_merkleRoot != 0);
+        merkleRoot = _merkleRoot;
+    }
+
+    function claim_rest_of_tokens_and_selfdestruct() public returns(bool) {
+        // only owner 
+        require(msg.sender == owner);
+        require(token_contract.balanceOf(address(this)) >= 0);
+        require(token_contract.transfer(owner, token_contract.balanceOf(address(this))));
+        selfdestruct(owner);
+        return true;
+    }
+
+    function addressToAsciiString(address x) internal pure returns (string) {
+        bytes memory s = new bytes(40);
+        for (uint i = 0; i < 20; i++) {
+            byte b = byte(uint8(uint(x) / (2**(8*(19 - i)))));
+            byte hi = byte(uint8(b) / 16);
+            byte lo = byte(uint8(b) - 16 * uint8(hi));
+            s[2*i] = char(hi);
+            s[2*i+1] = char(lo);            
+        }
+        return string(s);
+    }
+    
+    function char(byte b) internal pure returns (byte c) {
+        if (b < 10) return byte(uint8(b) + 0x30);
+        else return byte(uint8(b) + 0x57);
+    }
+
+
+    function uintToStr(uint i) internal pure returns (string){
+        if (i == 0) return "0";
+        uint j = i;
+        uint length;
+        while (j != 0){
+            length++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(length);
+        uint k = length - 1;
+        while (i != 0){
+            bstr[k--] = byte(48 + i % 10);
+            i /= 10;
+        }
+        return string(bstr);
+    }
+
+    function leaf_from_address_and_num_tokens(address _a, uint256 _n) public pure returns(bytes32 ) {
+        string memory prefix = "0x";
+        string memory space = " ";
+
+        // file with addresses and tokens have this format: "0x123...DEF 999", where 999 - num tokens
+        // function simply calculates hash of such a string, given the target adddres and num_tokens
         
-        if (!checkProof(proof, keccak256(who))) {
+        bytes memory _ba = bytes(prefix);
+        bytes memory _bb = bytes(addressToAsciiString(_a));
+        bytes memory _bc = bytes(space);
+        bytes memory _bd = bytes(uintToStr(_n));
+        string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length);
+        bytes memory babcde = bytes(abcde);
+        uint k = 0;
+        for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
+        for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
+        for (i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
+        for (i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
+
+        return bytes32(keccak256(abcde));
+    }
+
+
+    function mintByMerkleProof(bytes32[] _proof, address _who, uint256 _amount) public returns(bool) {
+        require(spent[_who] != true);
+        require(_amount > 0);
+        // require(msg.sender = _who); // makes not possible to mint tokens for somebody, uncomment for more strict version
+        
+        if (!checkProof(_proof, leaf_from_address_and_num_tokens(_who, _amount))) {
             return false;
         }
 
-        require(token_contract.balanceOf(address(this)) >= num_tokens_to_transfer);
+        spent[_who] = true;
 
-        spent[who] = true;
-
-        if (token_contract.transfer(who, num_tokens_to_transfer) == true) {
-            emit AirdropTransfer(msg.sender, num_tokens_to_transfer);
+        if (token_contract.transfer(_who, _amount) == true) {
+            emit AirdropTransfer(_who, _amount);
             return true;
         }
 
@@ -447,7 +531,7 @@ contract MintableToken is StandardToken, Ownable {
             }
         }
 
-        return h == merkle_root;
+        return h == merkleRoot;
     }
 }
 """
